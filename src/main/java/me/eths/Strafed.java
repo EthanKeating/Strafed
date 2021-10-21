d35a99af8801ba84c2511088ea4285922f7e5c96
EthsMC <69118918+EthsMC@users.noreply.github.com>
2021-10-20 22:15:03 -0400

    Add files via upload

---
+++ src/main/java/me/eths/Strafed.java
@@ -0,0 +1,15 @@
+package me.eths;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Strafed extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
